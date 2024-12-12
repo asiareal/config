c1f032b61e59699eddfe35d9d78b470ba14d0ca6
asiareal <asiawangreal@gmail.com>
2024-12-12 21:44:33 +0800

    fix: 🐛 服务端渲染

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -7,9 +7,12 @@ const options = {
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
-  return (
-    <PostHogProvider apiKey="phc_LtC9UZffjqeE4H1sylvX7gY0iTXilszwU5MEuVDxOWg" options={options}>
-      {children}
-    </PostHogProvider>
-  );
+  if (typeof window !== 'undefined') {
+    return (
+      <PostHogProvider apiKey="phc_LtC9UZffjqeE4H1sylvX7gY0iTXilszwU5MEuVDxOWg" options={options}>
+        {children}
+      </PostHogProvider>
+    );
+  }
+  return <>{children}</>;
 }
